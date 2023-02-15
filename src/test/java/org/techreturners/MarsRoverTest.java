8a9d772a9284531cd1ec7ec36e80b82a2b3ad118
Saranya Govind <saranya789@gmail.com>
2023-02-15 16:50:07 +0000

    Test case added for mars rover to move one grid from initial position

--- src/test/java/org/techreturners/MarsRoverTest.java
+++ src/test/java/org/techreturners/MarsRoverTest.java
@@ -1,9 +1,23 @@
 package org.techreturners;
 
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MarsRoverTest {
+
+    private Plateau plateau;
+
+    @BeforeEach
+    public void setUpPlateau() {
+        plateau = new Plateau(5, 5);
+    }
+
+    @Test
+    public void checkMarsRoverCanMoveOneGridPointIfFacingNorth() {
+        MarsRover marsRover = new MarsRover("0 0 N", plateau);
+    }
+
+
+
 }
