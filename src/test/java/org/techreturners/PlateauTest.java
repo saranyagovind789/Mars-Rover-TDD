17ea41e5e18e27254a23765893e6864e39283c77
Saranya Govind <saranya789@gmail.com>
2023-02-15 16:29:52 +0000

    Test case added for creating plateau of size 5*5

--- src/test/java/org/techreturners/PlateauTest.java
+++ src/test/java/org/techreturners/PlateauTest.java
@@ -1,55 +1,22 @@
 package org.techreturners;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 public class PlateauTest {
-    Plateau plateau = new Plateau(5,5);
-    @Test
-    public void checkIfInsideUpperXMaxTrue(){
-        Plateau plateau = new Plateau(5,5);
-        assertEquals(plateau.isInsideUpperXMax(5), true);
-    }
+
+
 
     @Test
-    public void checkIfInsideUpperXMaxFalse(){
-        Plateau plateau = new Plateau(5,5);
-        assertFalse(plateau.isInsideUpperXMax(6));
+    public void checkUserCanCreateAPlateauSizeFiveByFive() {
+        //Arrange
+        Plateau plateau = new Plateau(5, 5);
+        String expectedResult = "The plateau dimensions are 5 squares in the x direction and 5 squares in the y direction.";
+        //Act
+        String actual = plateau.getDimensions();
+        //Assert
+        Assertions.assertEquals(expectedResult, actual);
+
     }
 
-    @Test
-    public void checkIfInsideUpperYMaxTrue(){
-        Plateau plateau = new Plateau(5,5);
-        assertTrue(plateau.isInsideUpperYMax(4));
-    }
-
-    @Test
-    public void checkIfInsideUpperYMaxFalse(){
-        Plateau plateau = new Plateau(5,5);
-        assertFalse(plateau.isInsideUpperYMax(7));
-    }
-
-    @Test
-    public void checkIfInsideLowerXMaxTrue(){
-        Plateau plateau = new Plateau(5,5);
-        assertTrue(plateau.isInsideLowerXMax(2));
-    }
-
-    @Test
-    public void checkIfInsideLowerXMaxFalse(){
-        Plateau plateau = new Plateau(5,5);
-        assertFalse(plateau.isInsideLowerXMax(-2));
-    }
-
-    @Test
-    public void checkIfInsideLowerYMaxTrue(){
-        Plateau plateau = new Plateau(5,5);
-        assertTrue(plateau.isInsideLowerYMax(3));
-    }
-
-    @Test
-    public void checkIfInsideLowerYMaxFalse(){
-        Plateau plateau = new Plateau(5,5);
-        assertFalse(plateau.isInsideLowerYMax(-2));
-    }
 }
